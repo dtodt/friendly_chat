4038f29e719e48266aaba80801945dbc33e6d213
Daniel Otavio Todt <daniel.todt@projuris.com.br>
2021-12-07 19:01:09 -0300

    stateful convert

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,14 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
